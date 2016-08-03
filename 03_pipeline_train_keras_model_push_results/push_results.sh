ec17767a773073b6ee18199261516b18b6dce27a
ericwayman <ewayman@gmail.com>
2016-08-03 15:44:46 -0700

    Added directory 03_pipeline_train_keras_model_push_results to push the results of the model to a gist

---
+++ 03_pipeline_train_keras_model_push_results/push_results.sh
@@ -0,0 +1,15 @@
+set -e # fail fast
+set -x # print commands
+
+git clone resource-gist updated-gist
+clone resource-gist in directory: updated-gist
+
+cd updated-gist
+python /home/03_pipeline_train_keras_model_push_results/train_keras_mlp_predict.py > model_results
+echo model_results 
+git config --global user.email "ewayman@gmail.com"
+git config --global user.name "ericwayman"
+
+#commit model_results to repo
+git add .
+git commit -m "model_results"
