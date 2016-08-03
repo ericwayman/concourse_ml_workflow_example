bd418749732bbbcc004062ec8daf923b4b12a34c
ericwayman <ewayman@gmail.com>
2016-08-03 16:33:51 -0700

    Bug fix to push_results.sh.  Typo in updated_gist repo name

--- 03_pipeline_train_keras_model_push_results/push_results.sh
+++ 03_pipeline_train_keras_model_push_results/push_results.sh
@@ -3,9 +3,9 @@
 set -e # fail fast
 set -x # print commands
 
-git clone resource-gist updated-gist
+git clone resource-gist updated_gist
 
-cd updated-gist
+cd updated_gist
 python ../resource-files/03_pipeline_train_keras_model_push_results/train_keras_mlp_predict.py > model_results
 cat model_results 
 git config --global user.email "ewayman@gmail.com"
