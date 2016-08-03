c2958812f77d59f4a5bf2ffb49f54a3bad05693d
ericwayman <ewayman@gmail.com>
2016-08-03 16:11:53 -0700

    Bug fixes to push_results.sh

--- 03_pipeline_train_keras_model_push_results/push_results.sh
+++ 03_pipeline_train_keras_model_push_results/push_results.sh
@@ -1,12 +1,13 @@
+#!/bin/sh
+
 set -e # fail fast
 set -x # print commands
 
 git clone resource-gist updated-gist
-clone resource-gist in directory: updated-gist
 
 cd updated-gist
 python resource-files/03_pipeline_train_keras_model_push_results/train_keras_mlp_predict.py > model_results
-echo model_results 
+cat model_results 
 git config --global user.email "ewayman@gmail.com"
 git config --global user.name "ericwayman"
 
