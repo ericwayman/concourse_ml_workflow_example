8f67ad785d9fa571c084328a3d082bc7dd029fb5
ericwayman <ewayman@gmail.com>
2016-08-03 16:01:19 -0700

    pipeline and path bug fixes

--- 03_pipeline_train_keras_model_push_results/push_results.sh
+++ 03_pipeline_train_keras_model_push_results/push_results.sh
@@ -5,7 +5,7 @@ git clone resource-gist updated-gist
 clone resource-gist in directory: updated-gist
 
 cd updated-gist
-python /home/03_pipeline_train_keras_model_push_results/train_keras_mlp_predict.py > model_results
+python resource-files/03_pipeline_train_keras_model_push_results/train_keras_mlp_predict.py > model_results
 echo model_results 
 git config --global user.email "ewayman@gmail.com"
 git config --global user.name "ericwayman"
