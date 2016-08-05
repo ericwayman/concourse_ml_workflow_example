06f4f7a3fc09f8f5d975b3aa68c430b53bd9bb2d
ericwayman <ewayman@gmail.com>
2016-08-05 14:13:51 -0700

    Bug fixes to push_data_to_db.py to work with the pipeline set up

--- 04_pipeline_train_from_data_in_db/push_data_to_db.py
+++ 04_pipeline_train_from_data_in_db/push_data_to_db.py
@@ -5,15 +5,25 @@ import pandas as pd
 import numpy as np
 #from keras.datasets import mnist
 
+#{{database}}, {{host}}, {{port}}, {{user}}, {{password}}
 parser = argparse.ArgumentParser(description='File with database credentials')
-parser.add_argument('f', type=str,help='Full path to credential file')
+parser.add_argument('d', type=str,help='database')
+parser.add_argument('h', type=str,help='host')
+parser.add_argument('p', type=str,help='port')
+parser.add_argument('u', type=str,help='user')
+parser.add_argument('pw', type=str,help='password')
+
 
 NB_CLASSES = 10
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    credential_file = args.f
-    credentials = DBCredentials(credential_file)
+    database = args.d
+    host = args.h
+    port = args.p
+    user = args.u
+    password = args.pw
+    credentials = DBCredentials(database, host, port, user, password)
     engine = create_engine(credentials.sqlalchemy_connection_string)
     connection = engine.connect()
     #remove hard coding and take schema and table as arguments
