0dc9639392dcba392fd0434b21096e9de2d51b90
ericwayman <ewayman@gmail.com>
2016-08-11 15:27:43 -0700

    fixed print statemetns in set_table_name_environ_variables.py to work with python 3

--- 04_pipeline_train_from_data_in_db/set_table_name_environ_variables.py
+++ 04_pipeline_train_from_data_in_db/set_table_name_environ_variables.py
@@ -5,5 +5,5 @@ table_name_file = os.environ["TABLE_NAME_FILE"]
 with open(table_name_file,'r') as stream:
     table_name_dict =  yaml.load(stream)
 os.environ.update(table_name_dict)
-print os.environ
-print "success"
+print(os.environ)
+print("success")
