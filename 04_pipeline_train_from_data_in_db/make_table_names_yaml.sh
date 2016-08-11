9b98541ca98fd9b19df8c525de9a01eba0243836
Concourse <nobody@concourse.ci>
2016-08-11 10:44:03 -0700

    Added new working pipeline.yml.  Updated make_table_names_yaml.sh to properly write table names and schema

--- 04_pipeline_train_from_data_in_db/make_table_names_yaml.sh
+++ 04_pipeline_train_from_data_in_db/make_table_names_yaml.sh
@@ -6,7 +6,7 @@ set -x # print commands
 git clone train_test_tables_repo updated_train_test_tables_repo
 
 cd updated_train_test_tables_repo/
-echo -e "SCHEMA: $SCHEMA\nTRAIN_TABLE: $TRAIN_TABLE\nTEST_TABLE: $TEST_TABLE" > $TABLE_NAME_FILE
+echo "SCHEMA: $SCHEMA\nTRAIN_TABLE: $TRAIN_TABLE\nTEST_TABLE: $TEST_TABLE" > $TABLE_NAME_FILE
 
 git config --global user.email "nobody@concourse.ci"
 git config --global user.name "Concourse"
